ca19c422831169d4644156569214a73444a60791
meimolihan <meimolihan@gmail.com>
2025-03-15 14:36:39 +0800

    Site updated: 2025-03-15 14:36:38

--- js/复制提醒.js
+++ js/复制提醒.js
@@ -1,1 +1,38 @@
-document.addEventListener("copy",(function(e){new Vue({data:function(){return this.$notify({title:"哎嘿！复制成功",message:"若要转载请务必保留原文链接！猹分你个瓜！",position:"bottom-right",offset:50,showClose:!1,type:"success"}),{visible:!1}}})})),document.onkeydown=function(){if(window.event&&123==window.event.keyCode)return event.keyCode=0,event.returnValue=!1,new Vue({data:function(){return this.$notify({title:"啊啊！你干嘛啊！",message:"怎么能随随便便想扒猹的底裤呢？坏！",position:"bottom-right",offset:50,showClose:!1,type:"error"}),{visible:!1}}}),!1};
+// 复制提醒
+document.addEventListener("copy",function(e){
+  new Vue({
+    data:function(){
+      this.$notify({
+        title:"哎嘿！复制成功",
+        message:"若要转载请务必保留原文链接！猹分你个瓜！",
+        position: 'bottom-right',
+        offset: 50,
+        showClose: false,
+        type:"success"
+      });
+      return{visible:false}
+    }
+  })
+})
+
+/* 禁用F12按键并提醒 */
+document.onkeydown = function () {
+  if (window.event && window.event.keyCode == 123) {
+    event.keyCode = 0;
+    event.returnValue = false;
+    new Vue({
+      data:function(){
+        this.$notify({
+          title:"啊啊！你干嘛啊！",
+          message:"怎么能随随便便想扒猹的底裤呢？坏！",
+          position: 'bottom-right',
+          offset: 50,
+          showClose: false,
+          type:"error"
+        });
+        return{visible:false}
+      }
+    })
+    return false;
+  }
+};
