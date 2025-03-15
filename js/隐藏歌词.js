2a1cfc1895ef97f684a1876161a991de69bc43fa
meimolihan <meimolihan@gmail.com>
2025-03-15 14:33:36 +0800

    Site updated: 2025-03-15 14:33:34

--- js/隐藏歌词.js
+++ js/隐藏歌词.js
@@ -1,18 +1,1 @@
-// APlayer 默认关闭歌词
-// 创建一个 MutationObserver 实例，用于监听 DOM 的变化
-var observer = new MutationObserver(function (mutations) {
-  // 查找页面中 class 为 "aplayer-icon-lrc" 的元素
-  var lrcButton = document.querySelector(".aplayer-icon-lrc");
-  // 如果找到了 lrcButton
-  if (lrcButton) {
-    // 延迟1毫秒执行点击操作
-    setTimeout(function () {
-      lrcButton.click();
-    }, 1);
-    // 断开 MutationObserver 实例，停止监听 DOM 的变化
-    observer.disconnect();
-  }
-});
-
-// 开始观察 body 下的所有子节点及其属性变化
-observer.observe(document.body, { childList: true, subtree: true });
+var observer=new MutationObserver((function(e){var r=document.querySelector(".aplayer-icon-lrc");r&&(setTimeout((function(){r.click()}),1),observer.disconnect())}));observer.observe(document.body,{childList:!0,subtree:!0});
